072ff3d41f83d52e819db9915cc05c3e0a4b080c
ONG HAN LIN <onghanlin48@gmail.com>
2024-11-03 10:13:46 +0800

    Initial commit

---
+++ netlify/functions/sendEmail.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        return { statusCode: 405, body: 'Method Not Allowed' };
+    }
+
+    const { recipient, subject, message } = JSON.parse(event.body);
+
+    // Set up the transporter
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: "linwu212@gmail.com",
+            pass: "pdhktjzacpzzcqhd"
+        }
+    });
+
+    // Define mail options
+    const mailOptions = {
+        from: "linwu212@gmail.com",
+        to: recipient,
+        subject: subject,
+        text: message
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        return { statusCode: 200, body: 'Email sent successfully' };
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return { statusCode: 500, body: 'Error sending email' };
+    }
+};
